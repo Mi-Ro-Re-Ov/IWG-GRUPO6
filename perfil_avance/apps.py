cc6476998e3aca37ed85b8f5802777ff0f84c825
José Manuel Díaz <jdiazd@usm.cl>
2023-10-13 15:37:11 -0300

    Co-authored-by: Sebastian Avendaño Nogales (Alumno) <savendano@usm.cl>

--- perfil_avance/apps.py
+++ perfil_avance/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppTemperaturaConfig(AppConfig):
+class PerfilAvanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_temperatura'
+    name = 'perfil_avance'
