65d548894fab806f0539ff171d18f6fe30b06fa9
jdiazd <jdiazd@usm.cl>
2023-11-27 15:28:07 -0300

    Eliminación de elementos

--- ProyectoTemperatura/datos_curiosos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatosCuriososConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'datos_curiosos'
--- ProyectoTemperatura/datos_curiosos/models.py
+++
@@ -1,8 +0,0 @@
-# datos_curiosos/models.py
-from django.db import models
-
-class DatoCurioso(models.Model):
-    contenido = models.TextField()
-
-    def __str__(self):
-        return f"Dato Curioso - {self.id}"
